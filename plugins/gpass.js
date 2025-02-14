653eca645d58c10dc9fbef754a3310f4be4bc162
Mustaffamkm <kimongemustaffa@gmail.com>
2025-02-15 02:57:50 +0300

    ⭕POP KID⭕

--- plugins/gpass.js
+++ plugins/gpass.js
@@ -47,7 +47,7 @@ cmd({
     };
     const _0x51f475 = _0x571dd4(_0x1fe8bf);
     await _0x1f40d4.sendMessage(_0x329458, {
-      'text': "🔐 *Your Strong Password* 🔐\n\nPlease find your generated password below:\n\n *𝐌𝐮𝐬𝐭𝐚𝐟𝐟𝐚 𝐗𝐦𝐝*"
+      'text': "🔐 *Your Strong Password* 🔐\n\nPlease find your generated password below:\n\n *Toxic 𝐗𝐦𝐝*"
     }, {
       'quoted': _0x2b6900
     });
@@ -58,6 +58,6 @@ cmd({
     });
   } catch (_0x1ad7bb) {
     console.log(_0x1ad7bb);
-    _0x1bb557("❌𝐌𝐮𝐬𝐭𝐚𝐟𝐟𝐚 𝐗𝐦𝐝 Error generating password🤕: " + _0x1ad7bb.message);
+    _0x1bb557("❌Toxic 𝐗𝐦𝐝 Error generating password🤕: " + _0x1ad7bb.message);
   }
 });
