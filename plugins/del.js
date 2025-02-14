d291f81a7b65137279de86f4c7e8ef24ea08b695
Mustaffamkm <kimongemustaffa@gmail.com>
2025-02-15 02:33:10 +0300

    ⭕POP KID⭕

--- plugins/del.js
+++ plugins/del.js
@@ -23,6 +23,6 @@ const key = {
         await conn.sendMessage(m.chat, { delete: key })
 } catch(e) {
 console.log(e);
-reply('💫 Mᴜsᴛᴀғғᴀ xᴍᴅ 💫 successful..👨‍💻✅')
+reply('💫 TOXIC xᴍᴅ 💫 successful..👨‍💻✅')
 } 
 })
