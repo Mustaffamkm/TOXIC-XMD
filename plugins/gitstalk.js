6ae2a32cba849eb7688e7f8e3ebaf11b418caebf
Mustaffamkm <kimongemustaffa@gmail.com>
2025-02-15 03:03:39 +0300

    ⭕POP KID⭕

--- plugins/gitstalk.js
+++ plugins/gitstalk.js
@@ -27,7 +27,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 👥 *Followers*: ${data.followers} | Following: ${data.following}
 📅 *Created At*: ${new Date(data.created_at).toDateString()}
 🔭 *Public Gists*: ${data.public_gists}
-> © Toxic 𝐗𝐦𝐝 𝘔𝘋`;
+> © Toxic 𝐗𝐦d 𝘔𝘋`;
           const sentMsg = await conn.sendMessage(from,{image:{url: data.avatar_url },caption: userInfo },{quoted:mek })
     } catch (e) {
         console.log(e);
